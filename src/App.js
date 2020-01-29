d4c3e338526fd7ecbccf24fbbf9a4fdc55175715
Jordan Bonitatis <jordan.bonitatis@hulu.com>
2020-01-28 18:52:29 -0800

    don't reinvent the wheel; use unicef currency input

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+import Demo from './pages/Demo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">Open Fire</Typography>
+        </Toolbar>
+      </AppBar>
+      <Demo />
     </div>
   );
 }
