29d9783cdb04bbe8236882bdc4a593940d1ee098
trevor grayson <trevor@trevorgrayson.com>
2020-02-01 01:05:47 -0800

    numLabel

--- src/graphs/line.js
+++ src/graphs/line.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ChartistGraph from 'react-chartist';
 
-function numToLabel(value) {
-    return value.toString().length > 5 ? 
-        (value * 0.001).toString + 'k' : value
+function numLabel(value) {
+    let magnitudes = ' kMBT????'
+    value = value.toString()
+    let length = value.length
+    let zeros = 0
+
+    while (value[length-1 - zeros] === '0') {
+        zeros += 1
+    }
+
+    // getting rid of digit commas
+    zeros = zeros - (zeros % 3) 
+    let magnitude = magnitudes[zeros/3]
+
+    console.log(value, value.substring(0, length-zeros) + magnitude)
+    return value.substring(0, length-zeros) + magnitude
 }
 
 export class LineGraph extends React.Component {
@@ -24,14 +37,14 @@ export class LineGraph extends React.Component {
     var options = {
       axisX: {
         labelInterpolationFnc: function(value, index) {
-          return index % 2 === 0 ?  numToLabel(value) : null;
+          return index % 2 === 0 ?  numLabel(value) : null;
         }
       },
       axisY: {
         labelInterpolationFnc: function(value, index) {
           if (value === 0) { return 0 }
 
-          return index % 2 === 0 ?  numToLabel(value) : null;
+          return index % 2 === 0 ?  numLabel(value) : null;
         }
       }
     };
